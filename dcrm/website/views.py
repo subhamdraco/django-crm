c69e7ba45409ec58a8f2297473731ccb8d20b2b9
Subham Ghosh <subham.smart97@gmail.com>
2024-03-13 21:26:33 +0530

    added record model and template for view records

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 
 def home(request):
     # check to see logging in
+    records = Record.objects.all()
+
     if request.method == 'POST':
         # request.body for raw json data
         username = request.POST['user_name']
@@ -18,8 +22,8 @@ def home(request):
         else:
             messages.success(request, "Login Error! ")
             return redirect('home')
-
-    return render(request, 'home.html', {})
+    else:
+        return render(request, 'home.html', {'records': records})
 
 
 def logout_user(request):
@@ -29,4 +33,17 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
