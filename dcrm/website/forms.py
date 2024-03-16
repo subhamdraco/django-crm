965d2e192a65dda8bc14d7aecc0bc441ecb0bb6b
Subham Ghosh <subham.smart97@gmail.com>
2024-03-16 22:06:40 +0530

    f

--- dcrm/website/forms.py
+++ dcrm/website/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
 
 
 class SignUpForm(UserCreationForm):
@@ -21,17 +22,46 @@ class SignUpForm(UserCreationForm):
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['username'].widget.attrs['placeholder'] = 'username'
         self.fields['username'].label = ''
-        self.fields[
-            'username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
 
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['placeholder'] = 'Password'
         self.fields['password1'].label = ''
-        self.fields[
-            'password1'].help_text = '<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
+        self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
 
         self.fields['password2'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
         self.fields['password2'].label = ''
-        self.fields[
-            'password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
+        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
+
+
+# create add record
+class AddRecord(forms.ModelForm):
+    first_name = forms.CharField(required=True, label="",
+                                 widget=forms.TextInput(
+                                     attrs={'class': 'form-control', 'placeholder': 'First Name'}))
+    last_name = forms.CharField(required=True, label="",
+                                widget=forms.TextInput(
+                                    attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
+    address = forms.CharField(required=True, label="",
+                            widget=forms.TextInput(
+                                attrs={'class': 'form-control', 'placeholder': 'Address'}))
+    email = forms.CharField(required=True, label="",
+                            widget=forms.TextInput(
+                                attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    phone = forms.CharField(required=True, label="",
+                            widget=forms.TextInput(
+                                attrs={'class': 'form-control', 'placeholder': 'phone'}))
+    city = forms.CharField(required=True, label="",
+                           widget=forms.TextInput(
+                               attrs={'class': 'form-control', 'placeholder': 'city'}))
+    state = forms.CharField(required=True, label="",
+                            widget=forms.TextInput(
+                                attrs={'class': 'form-control', 'placeholder': 'state'}))
+    zip_code = forms.CharField(required=True, label="",
+                               widget=forms.TextInput(
+                                   attrs={'class': 'form-control', 'placeholder': 'zip_code'}))
+
+    class Meta:
+        model = Record
+        exclude = ('user',)
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecord
 from .models import Record
 
 
@@ -47,3 +47,59 @@ def register_user(request):
         form = SignUpForm()
         return render(request, 'register.html', {'form': form})
     return render(request, 'register.html', {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+
+        # lookup record
+        record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': record})
+
+    else:
+        messages.success(request, "You must be logged in to view! ")
+        return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+
+        # delete record
+        record_it = Record.objects.get(id=pk)
+        record_it.delete()
+        messages.success(request, "Record deleted successfully! ")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to delete record! ")
+        return redirect('home')
+
+
+def add_record(request):
+    form = AddRecord(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Record Added..")
+                return redirect('home')
+
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.success(request, "You must be logged in add record..")
+        return redirect('home')
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        to_update = Record.objects.get(pk=pk)
+        form = AddRecord(request.POST or None, instance=to_update)
+        if request.method == 'POST':
+            form.is_valid()
+            form.save()
+            messages.success(request, "Record is updated")
+            return redirect('home')
+        else:
+            return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.success(request, "You must be logged in add record..")
+        return redirect('home')
